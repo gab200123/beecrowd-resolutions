083a6b818e3e06a8bf3f77daaa92dc94957b607d
gab200123 <78562857+gab200123@users.noreply.github.com>
2025-03-19 08:29:37 -0300

    Update 1066_pares_impares_positivos_negativos.py
    I retired variables 'i', 'cont', 'paridade', with the objective of having cleaner code. Changed the 'while' loop to a 'for' loop, but is retorning "Wrong answer (10%)"

--- 1066_pares_impares_positivos_negativos.py
+++ 1066_pares_impares_positivos_negativos.py
@@ -1,35 +1,32 @@
 #The numbers should be entered with spaces to distinguish them as separate.
 n = list(map(float, input().split()))
-i = 0
 pos = 0
 neg = 0
-cont = 0
-paridade = 0
 par = 0
 impar = 0
 
-while i < len(n):
-  paridade = n[i] % 2
-  if n[i] > 0 and paridade == 0:
+for num in n:
+  if num > 0:
     pos += 1
-    par += 1
+    
+    if num % 2 == 0:
+      par += 1
 
-  elif n[i] > 0 and paridade > 0:
-    pos += 1
-    impar += 1
+    else:
+      impar += 1
 
-  elif n[i] < 0 and paridade == 0:
+  elif num < 0:
     neg += 1
-    par += 1
 
-  elif n[i] < 0 and paridade > 0:
-    neg += 1
-    impar += 1
+    if num % 2 == 0:
+      par += 1
+
+    else:
+      impar += 1
 
   else:
     par += 1
 
-  i += 1
 
 print(f'{par} valor(es) par(es)')
 print(f'{impar} valor(es) impar(es)')
